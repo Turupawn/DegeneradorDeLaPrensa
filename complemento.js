56a4c770bb8a7d0c3a15eca54a94e949103eb707
LuisBarahona <garabatos86@hotmail.com>
2016-06-24 22:14:45 -0600

    Update complemento.js

--- complemento.js
+++ complemento.js
@@ -6,4 +6,5 @@ complemento = [
 "la cura del cancer",
 "Silicon Valley de Latinoamérica",
 "máquina de educación para los más desfavorecidos",
+"escuela de potenciación de habilidades para las zonas rurales",
 "solución al hambre para los países del tercer mundo"];
