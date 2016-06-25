4c2c0ba49b5637a689783cb454e93334ad17a5b5
LuisBarahona <garabatos86@hotmail.com>
2016-06-24 22:27:12 -0600

    Update complemento.js

--- complemento.js
+++ complemento.js
@@ -7,4 +7,8 @@ complemento = [
 "Silicon Valley de Latinoamérica",
 "máquina de educación para los más desfavorecidos",
 "escuela de potenciación de habilidades para las zonas rurales",
+"picante método de crear dinero",
+"startup del futuro hoy",
+"despampanante tecnología",
+"futuro brillante en el presente",
 "solución al hambre para los países del tercer mundo"];
